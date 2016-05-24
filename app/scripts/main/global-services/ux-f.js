b5493b64b33938a20cf0e3500bf9eb69a6030f51
candaras <mark.candaras@gmail.com>
2016-05-23 21:06:58 -0400

    styles man

--- app/scripts/main/global-services/ux-f.js
+++ app/scripts/main/global-services/ux-f.js
@@ -6,17 +6,23 @@
  * Factory in the numetal.
  */
 angular.module('numetal')
-    .factory('Ux', function ($window)
+    .factory('Ux', function ($window, $rootScope)
     {
         'use strict';
 
         // INITIALIZATION
-
 
         // ACTUAL DEFINITION
         var service = {
             xy: [$window.innerWidth,$window.innerHeight]
         };
 
+        function adminUI(){
+            service.xy = [$window.innerWidth,$window.innerHeight]
+        }
+        angular.element($window).bind('resize', function () {
+            adminUI();
+            $rootScope.$apply(service.xy);
+        });
         return service;
     });
