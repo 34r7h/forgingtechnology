ebc55c1b693a09405e57a3362f7fefd53eb41eb9
candaras <mark.candaras@gmail.com>
2016-05-24 19:45:28 -0400

    html5 mode

--- web.js
+++ web.js
@@ -6,8 +6,8 @@ var express = require('express');
 var app = express();
 app.use(require('prerender-node').set('prerenderToken', 'ipMxPbev4DV5j1W9P7Mq'));
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/www"));
+// app.use(gzippo.staticGzip("" + __dirname + "/www"));
 app.use(function(req, res) {
-	res.sendfile(__dirname + '/www/index.html');
+	res.sendfile("" +__dirname + '/www/index.html');
 });
 app.listen(process.env.PORT || 5000);
